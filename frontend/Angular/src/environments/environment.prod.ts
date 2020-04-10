d2d5e23f7c76c313e19ec357094048fef11ad70c
Abhishek Gadag <47495127+AbhishekGadag@users.noreply.github.com>
2020-04-10 23:08:04 +0530

    first commit

---
+++ frontend/Angular/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  emailAPI: '',
+  database: 'graphql' ,
+  social: {
+    fblink: '',
+    linkedin: 'YYYYYYYY',
+    github: '',
+    emailid: ''
+  },
+  socialAuthEnabled: false,
+  graphql: 'http://localhost:3000/graphql'
+};
---
+++ frontend/Angular/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  emailAPI: '',
+  database: 'graphql' ,
+  social: {
+    fblink: '',
+    linkedin: 'YYYYYYYY',
+    github: '',
+    emailid: ''
+  },
+  socialAuthEnabled: false,
+  graphql: 'http://localhost:3000/graphql'
+};
